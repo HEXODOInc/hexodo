3ff676497c830053bda4c9318bddb94156c956f2
Cas de Reuver <casdr@0fi.net>
2016-03-08 14:41:27 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -L https://github.com/pyupio/statuspage/raw/master/dist/statuspage_linux > statuspage
-chmod +x statuspage
-./statuspage update --org=hexodoinc --name=status --token=$token
+curl -L https://github.com/pyupio/statuspage/raw/master/dist/statuspage_linux > ../statuspage
+chmod +x ../statuspage
+../statuspage update --org=hexodoinc --name=status --token=$token
